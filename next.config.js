cb3634e49019f1b85a92f75cd82c52bcb2c3303a
Bekzod <begzod0320@gmail.com>
2026-01-18 13:25:18 -0600

    fixed favicon

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
-    domains: ['expressway-towing.com'],
+    unoptimized: true,
   },
 }
 
